45ecec16537ba0d2b5e8cfc2d6f1418c05e95ddb
Chris Lin <chrisliny@gmail.com>
2015-03-09 22:43:18 -0700

    added unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/chris/projects/rails_postgresql_pagila"
+pid "/home/chris/projects/rails_postgresql_pagila/tmp/pids/unicorn.pid"
+stderr_path "/home/chris/projects/rails_postgresql_pagila/log/unicorn.log"
+stdout_path "/home/chris/projects/rails_postgresql_pagila/log/unicorn.log"
+
+listen "/tmp/unicorn.rails_postgresql_pagila.sock"
+worker_processes 2
+timeout 30
